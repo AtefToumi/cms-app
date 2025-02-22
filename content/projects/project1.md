2bb2d9bd0307b5de8451eb2f5e543783dabb5897
Atef <atef.toumi@ambient.digital>
2025-02-22 17:01:04 +0100

    initial commit

---
+++ content/projects/project1.md
@@ -0,0 +1,6 @@
+---
+title: "My First Project"
+description: "This is an awesome project!"
+image: "/uploads/project1.jpg"
+date: "2025-01-01"
+---
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import ProjectCard from "../components/ProjectCard";
+
+export default function Home({ projects }) {
+  return (
+    <div>
+      <h1>Student Organization Projects</h1>
+      <div className="projects-grid">
+        {projects.map((project) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const projectsDirectory = path.join(process.cwd(), "content/projects");
+  const filenames = fs.readdirSync(projectsDirectory);
+
+  const projects = filenames.map((filename) => {
+    const filePath = path.join(projectsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContents);
+    return { ...data };
+  });
+
+  return { props: { projects } };
+}
